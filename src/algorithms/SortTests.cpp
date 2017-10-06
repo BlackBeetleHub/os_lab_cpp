a9203505766c2fc24cb195dfb7a7b31ad58712b5
kirillafendi83 <kirillafendi83@gmail.com>
2017-10-07 01:35:43 +0300

    add clockTimer test and CommonTimer test

--- src/algorithms/SortTests.cpp
+++ src/algorithms/SortTests.cpp
@@ -3,7 +3,43 @@
 //
 
 #include <gtest/gtest.h>
+#include <memory>
+#include "timers/CommonTimer.h"
+#include "timers/ClockTimer.h"
+#include "algorithms/Sort.h"
+#include "easylogging++.h"
 
-TEST(CommonTest1, SpeedTest) {
-    ASSERT_TRUE(true);
+class SpeedTest : public ::testing::Test {
+public:
+
+    void setAlgorithm(std::shared_ptr<Algorithm> algorithm){
+        this->algorithm = algorithm;
+    }
+
+    void setTimer(std::shared_ptr<Timer> timer){
+        this->timer = timer;
+    }
+
+    virtual void SetUp() { }
+
+    virtual void TearDown() {
+        LOG(INFO) << "Start testing...";
+        time_t start = timer->start();
+        algorithm->execute();
+        time_t end =  timer->end();
+        LOG(INFO) << "Time: " << end - start;
+    }
+
+    std::shared_ptr<Algorithm> algorithm;
+    std::shared_ptr<Timer> timer;
+};
+
+TEST_F(SpeedTest, ClockTimerSort) {
+    setAlgorithm(std::make_shared<Sort>(100000));
+    setTimer(std::make_shared<ClockTimer>());
+}
+
+TEST_F(SpeedTest, CommonTimerSort) {
+    setAlgorithm(std::make_shared<Sort>(100000));
+    setTimer(std::make_shared<CommonTimer>());
 }
