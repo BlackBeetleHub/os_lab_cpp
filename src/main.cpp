a5aff62558071490ed4841b1dca3b37043f0d6fd
kirillafendi83 <kyrylo.afendi@nure.ua>
2017-11-08 22:41:25 +0200

    change imlp SpeedTest test_fixture

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <easylogging++.h>
 
-int main(int argc, char** argv) {
-    START_EASYLOGGINGPP(argc, argv);
+int main() {
+    //START_EASYLOGGINGPP(argc, argv);
     return 0;
 }
