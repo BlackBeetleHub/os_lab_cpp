8351e7e02a063beeaf11fee3f26362c126b10dd7
kirillafendi83 <kyrylo.afendi@nure.ua>
2017-11-19 15:15:16 +0200

    done 3 lab

--- src/algorithms/CustomArray.h
+++ src/algorithms/CustomArray.h
@@ -8,14 +8,19 @@
 #include <cstddef>
 #include <random>
 #include <memory>
+#include <easylogging++.h>
+#include <timers/OmpTimer.h>
 
 template<typename T>
 class CustomArray {
 public:
     explicit CustomArray(size_t size) : size(size) {
         array = new T[size];
+    }
+
+    void randInit(){
         for (int i = 0; i < size; i++) {
-            array[i] = random() % 100;
+            array[i] = random() % 10000;
         }
     }
 
@@ -23,11 +28,92 @@ public:
 
     pointer operator+(CustomArray& second) {
         pointer result = std::make_shared<CustomArray<T>>(second.size);
+        timer->start();
         for (int i = 0; i < second.size; i++) {
             result->array[i] = second[i] + (*this)[i];
         }
+        timer->end();
+        LOG(INFO) << timer->show();
         return result;
     }
+
+    template<typename S>
+    pointer SIMDAddition(CustomArray& second, S (*add)(S,S)) {
+        if(second.size != this->size) {
+            throw std::runtime_error("array not equal size");
+        }
+        auto *ma = (S*)second.array;
+        auto *mb = (S*)this->array;
+        pointer result = std::make_shared<CustomArray<T>>(second.size);
+        size_t del = sizeof(T);
+        size_t len = second.size / (16 / del);
+        auto *mout = new S[len];
+        timer->start();
+        for (int i = 0; i < len; i++) {
+            mout[i] = add(ma[i], mb[i]);
+        }
+        timer->end();
+        LOG(INFO) << timer->show();
+        result->array = (T*)mout;
+        return result;
+    }
+
+    pointer fastSIMDAddition(CustomArray& second){
+        if(second.size != this->size) {
+            throw std::runtime_error("array not equal size");
+        }
+        __m128i *ma = (__m128i*)second.array;
+        __m128i *mb = (__m128i*)this->array;
+        pointer result = std::make_shared<CustomArray<T>>(second.size);
+        size_t del = sizeof(T);
+        size_t len = second.size / (16 / del);
+        __m128i *mout = new __m128i[len];
+        timer->start();
+        for (int i = 0; i < len; i++) {
+            mout[i] = _mm_add_epi64(ma[i], mb[i]);
+        }
+        timer->end();
+        LOG(INFO) << timer->show();
+        result->array = (T*)mout;
+        return result;
+    }
+
+    pointer Sqrt(){
+        pointer result = std::make_shared<CustomArray<T>>(size);
+        timer->start();
+        for (int i = 0; i < size; i++) {
+            result->array[i] = sqrt((*this)[i]);
+        }
+        timer->end();
+        LOG(INFO) << timer->show();
+        return result;
+    }
+
+    template<typename S>
+    pointer SIMDSqrt(S (*sqrt_mm)(S)) {
+        auto *ma = (S*)this->array;
+        pointer result = std::make_shared<CustomArray<T>>(size);
+        size_t del = sizeof(T);
+        size_t len = size / (16 / del);
+        auto *mout = new S[len];
+        timer->start();
+        for (int i = 0; i < len; i++) {
+            mout[i] = sqrt_mm(ma[i]);
+        }
+        timer->end();
+        LOG(INFO) << timer->show();
+        result->array = (T*)mout;
+        return result;
+    }
+
+    T getSum(){
+        T res = 0;
+        for(size_t i = 0; i < this->size; i++) {
+            res+= (*this)[i];
+        }
+        return res;
+    }
+
 
     T operator[](int index) {
         return this->array[index];
@@ -40,7 +126,47 @@ public:
 private:
     T *array;
     size_t size;
+    std::shared_ptr<Timer> timer = std::make_shared<OmpTimer>();
 };
+
+
+#define SHL128(v, n) \
+({ \
+    __m128i v1, v2; \
+ \
+    if ((n) >= 64) \
+    { \
+        v1 = _mm_slli_si128(v, 8); \
+        v1 = _mm_slli_epi64(v1, (n) - 64); \
+    } \
+    else \
+    { \
+        v1 = _mm_slli_epi64(v, n); \
+        v2 = _mm_slli_si128(v, 8); \
+        v2 = _mm_srli_epi64(v2, 64 - (n)); \
+        v1 = _mm_or_si128(v1, v2); \
+    } \
+    v1; \
+})
+
+#define SHR128(v, n) \
+({ \
+    __m128i v1, v2; \
+ \
+    if ((n) >= 64) \
+    { \
+        v1 = _mm_srli_si128(v, 8); \
+        v1 = _mm_srli_epi64(v1, (n) - 64); \
+    } \
+    else \
+    { \
+        v1 = _mm_srli_epi64(v, n); \
+        v2 = _mm_srli_si128(v, 8); \
+        v2 = _mm_slli_epi64(v2, 64 - (n)); \
+        v1 = _mm_or_si128(v1, v2); \
+    } \
+    v1; \
+})
 
 
 #endif //LAB_OS_1_CUSTOMARRAY_H
--- src/algorithms/SortTests.cpp
+++ src/algorithms/SortTests.cpp
@@ -2,8 +2,6 @@
 // Created by kirill on 06.10.17.
 //
 
-#include "test/Test.h"
-
 #include "timers/CommonTimer.h"
 #include "timers/ClockTimer.h"
 #include "timers/ChronoTimer.h"
@@ -11,17 +9,168 @@
 #include "timers/OmpTimer.h"
 #include "CustomArray.h"
 
-TEST_F(Test, OmpTimerSort) {
-    setCommands([](){std::cout<<"hello"<<std::endl;});
-    setTimer(std::make_shared<OmpTimer>());
+#include <functional>
+#include <gtest/gtest.h>
+
+#include <timers/Timer.h>
+
+template<typename T, typename A>
+void CustomArrayTestF(size_t size){
+    auto timer = std::make_shared<T>();
+    CustomArray<A> a1(size);
+    a1.randInit();
+    CustomArray<A> a2(size);
+    a2.randInit();
+    timer->start();
+    auto res = a1 + a2;
+    timer->end();
+    LOG(INFO) << timer->show();
+}
+
+TEST(Testa, OmpTimerSorta) {
+    CustomArrayTestF<OmpTimer, int16_t >(4096*4096);
+}
+
+TEST(Testa, OmpTimerSorta2) {
+    CustomArrayTestF<OmpTimer, int32_t>(4096*4096);
+}
+
+TEST(Testa, OmpTimerSorta23) {
+    CustomArrayTestF<OmpTimer, uint64_t >(4096*4096);
+}
+
+TEST(Testa, OmpTimerSorta231) {
+    CustomArrayTestF<OmpTimer, uint_least64_t >(4096*4096);
+}
+
+TEST(SSETest, DoubleAddition) {
+    auto timer = std::make_shared<OmpTimer>();
+    CustomArray<double> a1(4096);
+    a1.randInit();
+    CustomArray<double> a2(4096);
+    a2.randInit();
+    timer->start();
+    auto res = a1.SIMDAddition<__m128d>(a2, [](__m128d a, __m128d b){
+        return _mm_add_pd(a,b);
+    });
+    timer->end();
+    LOG(INFO) << timer->show();
+    auto expect = a1.getSum() + a2.getSum();
+    auto actual = res->getSum();
+    ASSERT_TRUE(expect == actual);
+}
+
+template<typename T>
+T add(T a , T b){
+    return _mm_add_epi64(a, b);
 }
 
 
-TEST_F(Test, OmpTimerSort1) {
-    setCommands([](){
-        CustomArray<int> a(4096*4096);
-        CustomArray<int> b(4096*4096);
-        CustomArray<int>::pointer c = a + b;
-    });
-    setTimer(std::make_shared<OmpTimer>());
+/*Note: It can be slowed if we use std::function instead of function pointer*/
+TEST(SSETest, Uint64Addition) {
+    auto timer = std::make_shared<OmpTimer>();
+    CustomArray<uint64_t> a1(4096 * 4096);
+    a1.randInit();
+    CustomArray<uint64_t> a2(4096 * 4096);
+    a2.randInit();
+    timer->start();
+    auto res = a1.SIMDAddition<__m128i>(a2, add<__m128i>);
+    timer->end();
+    LOG(INFO) << timer->show();
+    auto expect = a1.getSum() + a2.getSum();
+    auto actual = res->getSum();
+    ASSERT_TRUE(expect == actual);
+}
+
+TEST(SSETest, Uint64Addition1) {
+    auto timer = std::make_shared<OmpTimer>();
+    CustomArray<uint64_t> a1(4096 * 4096);
+    a1.randInit();
+    CustomArray<uint64_t> a2(4096 * 4096);
+    a2.randInit();
+    timer->start();
+    auto res = a1.fastSIMDAddition(a2);
+    timer->end();
+    LOG(INFO) << timer->show();
+    auto expect = a1.getSum() + a2.getSum();
+    auto actual = res->getSum();
+    ASSERT_TRUE(expect == actual);
+}
+
+
+TEST(SqrtTests, CommonWithFloat) {
+    CustomArray<float> a1(4096 * 4096);
+    a1.randInit();
+    auto res = a1.Sqrt();
+}
+
+TEST(SqrtTests, CommonWithDouble) {
+    CustomArray<double> a1(4096 * 4096);
+    a1.randInit();
+    auto res = a1.Sqrt();
+}
+
+template<typename T>
+T sqrt_mm_f(T a){
+    return _mm_sqrt_ps(a);
+}
+
+TEST(SqrtTests, SIMDSqrtFloat) {
+    CustomArray<float> a1(4096 * 4096);
+    a1.randInit();
+    auto res = a1.SIMDSqrt(sqrt_mm_f<__m128>);
+}
+
+template<typename T>
+T sqrt_mm_d(T a){
+    return _mm_sqrt_pd(a);
+}
+
+TEST(SqrtTests, SIMDSqrtDouble) {
+    CustomArray<double> a1(4096 * 4096);
+    a1.randInit();
+    //_mm_shl_epi16() - move
+    auto res = a1.SIMDSqrt(sqrt_mm_d<__m128d>);
+}
+
+TEST(MoveTests, SIMDMove) {
+   /* int *a = new int[4] { 1,2, 3, 4};
+    int *b = new int[4] { 1,2, 3, 4};
+
+    __m128i ma = _mm_load_si128((__m128i*)a);
+    __m128i mb = _mm_load_si128((__m128i*)b);
+
+    __m128i res = _mm_shl_epi32(ma, mb);
+
+    int* r = (int*)&res;
+
+    LOG(INFO) << r[1];
+    LOG(INFO) << r[2];
+    LOG(INFO) << r[3];*/
+
+    //__m128d di = (__m128d)i;
+
+//    _mm_shl_epi32
+    //_mm_shl_epi16() - move
+}
+
+TEST(MoveTests, CommonMove) {
+    __m128i va = _mm_setr_epi8(0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f);
+    __m128i vb, vc;
+
+    vb = SHL128(va, 4);
+    vc = SHR128(va, 4);
+
+    int *resa = (int*)&va;
+    LOG(INFO) << "-----------";
+    LOG(INFO) << (resa[0] << 63);
+    LOG(INFO) << resa[1];
+    LOG(INFO) << resa[2];
+    LOG(INFO) << resa[3];
+    LOG(INFO) << "-----------";
+    int *resb = (int*)&vb;
+    LOG(INFO) << resb[0];
+    LOG(INFO) << resb[1];
+    LOG(INFO) << resb[2];
+    LOG(INFO) << resb[3];
 }
